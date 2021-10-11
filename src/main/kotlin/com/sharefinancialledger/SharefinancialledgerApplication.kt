9f8198454a5950ebb82293ad5c9b6b710dcbe0d3
root-kjh <wldnro3141@gmail.com>
2021-10-11 20:47:59 +0900

    Initial commit

---
+++ src/main/kotlin/com/sharefinancialledger/SharefinancialledgerApplication.kt
@@ -0,0 +1,11 @@
+package com.sharefinancialledger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharefinancialledgerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SharefinancialledgerApplication>(*args)
+}
---
+++ src/test/kotlin/com/sharefinancialledger/SharefinancialledgerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sharefinancialledger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SharefinancialledgerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
