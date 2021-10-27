fc548560c6cf77957a7b29b671c15a77302161f5
lowelllll <yejin2651@naver.com>
2021-10-28 01:51:10 +0900

    imp: 회원가입 성공시 http status 201 반환

--- src/main/kotlin/com/sharefinancialledger/user/controller/UserController.kt
+++ src/main/kotlin/com/sharefinancialledger/user/controller/UserController.kt
@@ -2,10 +2,12 @@ package com.sharefinancialledger.user.controller
 
 import com.sharefinancialledger.user.controller.dto.CreateUserRequest
 import com.sharefinancialledger.user.service.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
 @RequestMapping("/api")
@@ -14,7 +16,8 @@ class UserController(
 ) {
 
     @PostMapping("/v1/user")
-    fun create(@RequestBody request: CreateUserRequest) {
+    fun create(@RequestBody request: CreateUserRequest): ResponseEntity<Unit> {
         service.create(request)
+        return ResponseEntity.created(URI("")).build()
     }
 }
