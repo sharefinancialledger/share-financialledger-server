84781769cc67ca54c3296f21bab6b7c85e2e6484
lowelllll <yejin2651@naver.com>
2021-10-28 23:23:40 +0900

    feat: 회원가입 API path 변경

--- src/main/kotlin/com/sharefinancialledger/user/controller/UserController.kt
+++ src/main/kotlin/com/sharefinancialledger/user/controller/UserController.kt
@@ -15,7 +15,7 @@ class UserController(
         private val service: UserService
 ) {
 
-    @PostMapping("/v1/user")
+    @PostMapping("/v1/users")
     fun create(@RequestBody request: CreateUserRequest): ResponseEntity<Unit> {
         service.create(request)
         return ResponseEntity.created(URI("")).build() // TODO 유저 정보 조회 API 개발 시 URI 넣을 예정
