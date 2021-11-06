f6673e03f0c21ffaffb3883854133813fc268a4c
root-kjh <wldnro3141@gmail.com>
2021-11-07 01:34:03 +0900

    DEV-26 rename CategoryRepository.kt to SubCategoryRepository.kt

--- src/main/kotlin/com/sharefinancialledger/domain/subcategory/repository/CategoryRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.sharefinancialledger.domain.subcategory.repository
-
-import com.sharefinancialledger.domain.subcategory.entity.SubCategory
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SubCategoryRepository : JpaRepository<SubCategory, Int>
