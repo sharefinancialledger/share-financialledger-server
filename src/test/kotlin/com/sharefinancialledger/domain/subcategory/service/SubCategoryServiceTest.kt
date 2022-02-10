8f096eb1ae00ce9b8bb3e39a8b548df6e68d23d3
root-kjh <wldnro3141@gmail.com>
2022-02-10 21:47:28 +0900

    test: update subcategory test

--- src/test/kotlin/com/sharefinancialledger/domain/subcategory/service/SubCategoryServiceTest.kt
+++ src/test/kotlin/com/sharefinancialledger/domain/subcategory/service/SubCategoryServiceTest.kt
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SubCategoryServiceTest {
 
@@ -24,7 +26,7 @@ class SubCategoryServiceTest {
     private val userId = 100
 
     @Test
-    fun `서브카테고리를 생성한다`() {
+    fun `create sub category`() {
         val request = CreateSubCategoryRequest("카테고리", 10)
         val subCategory = SubCategory(userId = userId, title = request.title, categoryId = request.category)
         given(repository.save(argThat { it.userId == userId && it.title == request.title && it.categoryId == request.category }))
@@ -35,19 +37,19 @@ class SubCategoryServiceTest {
     }
 
     @Test
-    fun `return subcategories by userId`() {
+    fun `return subcategory by userId and subCategoryId`() {
         var userId = 1
         val subCategories = ArrayList<SubCategory>()
-        subCategories.add(SubCategory(userId = userId, title = "testOne", categoryId = 1))
-        subCategories.add(SubCategory(userId = userId, title = "testTwo", categoryId = 1))
+        subCategories.add(SubCategory(id=0,userId = userId, title = "testOne", categoryId = 1))
+        subCategories.add(SubCategory(id=1, userId = userId, title = "testTwo", categoryId = 1))
 
-        given(repository.findAllByUserId(userId))
-                .willReturn(subCategories)
+        given(repository.findById(0))
+                .willReturn(Optional.of(subCategories[0]))
+        given(repository.findById(1))
+                .willReturn(Optional.of(subCategories[1]))
 
-        val resultCategory = service.get(userId)
-        assertThat(resultCategory[0]).isEqualTo(subCategories[0])
-        assertThat(resultCategory[1]).isEqualTo(subCategories[1])
-
+        val resultSubCategory = service.findOwn(0, userId)
+        assertThat(resultSubCategory).isEqualTo(subCategories[0])
     }
 
     @Test
@@ -55,11 +57,22 @@ class SubCategoryServiceTest {
         val subCategoryId = 1
         val targetSubCategoryTitle = "target subcategory"
         val beforeUpdateSubCateogry = SubCategory(id = 1, userId = 1, title = "source subcategory", categoryId = 1)
-        given(repository.getById(subCategoryId))
-                .willReturn(beforeUpdateSubCateogry)
-        val afterUpdateSubCategory = service.update(subCategoryId, UpdateInfoRequest(targetSubCategoryTitle))
+        given(repository.findById(subCategoryId))
+                .willReturn(Optional.of(beforeUpdateSubCateogry))
+        val afterUpdateSubCategory = service.update(1, subCategoryId, UpdateInfoRequest(targetSubCategoryTitle))
 
         beforeUpdateSubCateogry.title = targetSubCategoryTitle
         assertThat(afterUpdateSubCategory).isEqualTo(beforeUpdateSubCateogry)
+    }
+
+    @Test
+    fun `delete subCategory`() {
+        var userId = 1
+        val subCategory = SubCategory(id=0,userId = userId, title = "testOne", categoryId = 1)
+        given(repository.findById(0))
+                .willReturn(Optional.of(subCategory))
+        service.delete(1, 0)
+        assertThat(subCategory.deletedAt).isNotNull()
+
     }
 }
